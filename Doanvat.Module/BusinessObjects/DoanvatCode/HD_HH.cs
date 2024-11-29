95a6ec708f67f382249f77ce1a27363b5421edaf
AnhPhuc09 <phannphucc@gmail.com>
2024-11-29 16:41:09 +0700

    init project

---
+++ Doanvat.Module/BusinessObjects/DoanvatCode/HD_HH.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace Doanvat.Module.Doanvat
+{
+
+    public partial class HD_HH
+    {
+        public HD_HH(Session session) : base(session) { }
+        public override void AfterConstruction() {
+            base.AfterConstruction();
+            if (Session.IsNewObject(This))
+            {
+
+            }
+        }
+    }
+
+}
